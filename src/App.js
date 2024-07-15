10492697c84de443752abe7e0aaee8d1eb4dd01b
Felipe <felipe.machado8437@gmail.com>
2024-07-15 11:32:56 -0300

    Adding divs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <div className="calculator">
+          <div className="formula"></div>
+          <div className="output"></div>
+          <div className="buttons">
+            <button className="btn"></button>
+            <button className="btn"></button>
+            <button className="btn"></button>
+            <button className="btn"></button>
+            <button className="btn"></button>
+            <button className="btn"></button>
+            <button className="btn"></button>
+            <button className="btn"></button>
+            <button className="btn"></button>
+            <button className="btn"></button>
+            <button className="btn"></button>
+            <button className="btn"></button>
+            <button className="btn"></button>
+            <button className="btn"></button>
+            <button className="btn"></button>
+            <button className="btn"></button>
+            <button className="btn" id="equals" value="=">=</button>
+          </div>
+        </div>
+        <div className="author"></div>
+      </div>
     </div>
   );
 }
